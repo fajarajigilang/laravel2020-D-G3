056211ea3f684cce18f9018e5326fca13a00f1db
wiwit <waulia25@gmail.com>
2020-04-06 19:36:51 +0700

    wiwit_aulia_rahman_17090080_6D

---
+++ Tugaske-6/app/Http/Controllers/TransaksiController.php
@@ -0,0 +1,86 @@
+<?php
+
+namespace App\Http\Controllers;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Http\Request;
+
+class TransaksiController extends Controller
+{
+       public function transaksi()
+       {
+          // mengambil data dari table pegawai
+    	$transaksi = DB::table('transaksi')->paginate(10);
+ 
+    	// mengirim data pegawai ke view index
+    	return view('transaksi',['transaksi' => $transaksi]);
+
+       }
+       
+       public function tambah()
+       {
+ 
+	      // memanggil view tambah
+	      return view('transaksi_tambah');
+ 
+        }
+
+        public function store(Request $request)
+        {
+	// insert data ke table pegawai
+	DB::table('transaksi')->insert([
+		'pelanggan_id' => $request->pelanggan_id,
+		'sopir_id' => $request->sopir_id,
+        'transaksi_tglmulai' => $request->transaksi_tglmulai,
+        'transaksi_tglselesai' => $request->transaksi_tglselesai,
+        'transaksi_hari' => $request->transaksi_hari,
+        'transaksi_tglovertime' => $request->transaksi_tglovertime,
+        'transaksi_hariovertime' => $request->transaksi_hariovertime,
+        'transaksi_total' => $request->transaksi_total,
+        'transaksi_status' => $request->transaksi_status
+	]);
+	// alihkan halaman ke halaman pegawai
+	return redirect('/transaksi');
+ 
+         }
+public function edit($transaksi_id)
+{
+	// mengambil data pegawai berdasarkan id yang dipilih
+	$transaksi = DB::table('transaksi')->where('transaksi_id',$transaksi_id)->get();
+	// passing data pegawai yang didapat ke view edit.blade.php
+	return view('transaksi_edit',['transaksi' => $transaksi]);
+ 
+}
+
+public function update(Request $request)
+{
+	// update data pegawai
+	DB::table('transaksi')->where('transaksi_id',$request->transaksi_id)->update([
+		'pelanggan_id' => $request->pelanggan_id,
+		'sopir_id' => $request->sopir_id,
+        'transaksi_tglmulai' => $request->transaksi_tglmulai,
+        'transaksi_tglselesai' => $request->transaksi_tglselesai,
+        'transaksi_hari' => $request->transaksi_hari,
+        'transaksi_tglovertime' => $request->transaksi_tglovertime,
+        'transaksi_hariovertime' => $request->transaksi_hariovertime,
+        'transaksi_total' => $request->transaksi_total,
+        'transaksi_status' => $request->transaksi_status
+	]);
+	// alihkan halaman ke halaman pegawai
+	return redirect('/transaksi');
+}
+
+public function cari(Request $request)
+	{
+		// menangkap data pencarian
+		$cari = $request->cari;
+ 
+    		// mengambil data dari table pegawai sesuai pencarian data
+		$transaksi = DB::table('transaksi')
+		->where('transaksi_tglselesai','like',"%".$cari."%")
+		->paginate();
+ 
+    		// mengirim data pegawai ke view index
+		return view('transaksi',['transaksi' => $transaksi]);
+ 
+	}
+}
